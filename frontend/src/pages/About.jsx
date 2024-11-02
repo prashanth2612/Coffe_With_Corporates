9f128e1a540add3cdde448d412e02cdd90a8ae9e
Prashanth Goud <145241730+prashanth2612@users.noreply.github.com>
2024-11-02 22:28:55 +0530

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -11,12 +11,12 @@ const About = () => {
       extra={
         <>
           <p>
-            GitHub Profile: <a href="https://github.com/prashanth2612!" target="_blank" rel="noopener noreferrer">https://github.com/prashanth2612!</a>
+            GitHub Profile: <a href="https://github.com/prashanth2612" target="_blank" rel="noopener noreferrer">https://github.com/prashanth2612</a>
           </p>
           <Button
             type="primary"
             onClick={() => {
-              window.open(`https://github.com/prashanth2612!`);
+              window.open(`https://github.com/prashanth2612`);
             }}
           >
             {translate('Visit GitHub Profile')}
