042061bff317f5f0a8ebaedfdd05694430a2adbe
Prashanth Goud <145241730+prashanth2612@users.noreply.github.com>
2024-11-02 22:17:39 +0530

    Update ErpOs.jsx

--- frontend/src/apps/ErpOs.jsx
+++ frontend/src/apps/ErpOs.jsx
@@ -1,12 +1,8 @@
-import { lazy, Suspense, useEffect, useState } from 'react';
+import { lazy, Suspense } from 'react';
 
-import { useSelector } from 'react-redux';
-import { selectAuth } from '@/redux/auth/selectors';
 import { AppContextProvider } from '@/context/appContext';
 import PageLoader from '@/components/PageLoader';
-import AuthRouter from '@/router/AuthRouter';
 import Localization from '@/locale/Localization';
-import { notification } from 'antd';
 
 const ErpApp = lazy(() => import('./ErpApp'));
 
@@ -21,53 +17,9 @@ const DefaultApp = () => (
 );
 
 export default function ErpOs() {
-  const { isLoggedIn } = useSelector(selectAuth);
-
   console.log(
-    '🚀 Welcome to COFEE WITH CORPORATES! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
+    '🚀 Welcome to COFFEE WITH CORPORATES! Did you know that we also offer commercial customization services? Contact us at coffe with corporates.com for more information.'
   );
 
-  // // Online state
-  // const [isOnline, setIsOnline] = useState(navigator.onLine);
-
-  // useEffect(() => {
-  //   // Update network status
-  //   const handleStatusChange = () => {
-  //     setIsOnline(navigator.onLine);
-  //     if (!isOnline) {
-  //       console.log('🚀 ~ useEffect ~ navigator.onLine:', navigator.onLine);
-  //       notification.config({
-  //         duration: 20,
-  //         maxCount: 1,
-  //       });
-  //       // Code to execute when there is internet connection
-  //       notification.error({
-  //         message: 'No internet connection',
-  //         description: 'Cannot connect to the Internet, Check your internet network',
-  //       });
-  //     }
-  //   };
-
-  //   // Listen to the online status
-  //   window.addEventListener('online', handleStatusChange);
-
-  //   // Listen to the offline status
-  //   window.addEventListener('offline', handleStatusChange);
-
-  //   // Specify how to clean up after this effect for performance improvment
-  //   return () => {
-  //     window.removeEventListener('online', handleStatusChange);
-  //     window.removeEventListener('offline', handleStatusChange);
-  //   };
-  // }, [navigator.onLine]);
-
-  if (!isLoggedIn)
-    return (
-      <Localization>
-        <AuthRouter />
-      </Localization>
-    );
-  else {
-    return <DefaultApp />;
-  }
+  return <DefaultApp />;
 }
